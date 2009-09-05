b2aa174aa89c9f546580d3acbd194c367824c2eb
Luke <github@lukesmith.net>
2009-09-05 14:56:18 +0100

    Added code from subversion

---
+++ src/Examples/Datasets.js
@@ -0,0 +1,7 @@
+﻿var people = [];
+people.push({ FirstName: "John", LastName: "Doe", Gender: "Male", Age: 53, Job: "Developer" });
+people.push({ FirstName: "Mary", LastName: "Whitehouse", Gender: "Female", Age: 23, Job: "Developer" });
+people.push({ FirstName: "Samantha", LastName: "Jones", Gender: "Female", Age: 16, Job: "Student" });
+people.push({ FirstName: "Frank", LastName: "Bennet", Gender: "Male", Age: 36, Job: "Accountant" });
+people.push({ FirstName: "Geoff", LastName: "Roberts", Gender: "Male", Age: 23, Job: "Prime Minister" });
+people.push({ FirstName: "Todd", LastName: "Jones", Gender: "Male", Age: 55, Job: "Retired" });
---
+++ tools/packer/README.txt
@@ -0,0 +1,11 @@
+SmallSharpTools.Packer
+
+Javascript Compressor featuring Packer, JSMin, CSSMin and Combine
+
+http://dean.edwards.name/packer/
+http://www.crockford.com/javascript/jsmin.html
+http://www.dimebrain.com/2008/03/a-better-css-mi.html
+
+Re-packaged and published by SmallSharpTools.com.
+
+http://www.smallsharptools.com/
